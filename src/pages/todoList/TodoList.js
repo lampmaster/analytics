4f68acb7e06dbb29d5f321cbd869a3dc0a98f447
Nik <paperslink@gmail.com>
2021-04-11 13:06:27 +0300

    chart added

--- src/pages/todoList/TodoList.js
+++ src/pages/todoList/TodoList.js
@@ -1,14 +1,23 @@
 import React, {Component} from "react";
 import {Task} from "../../components/Task/Task";
 import {Chart} from "../../components/Chart/Chart";
+import {connect} from "react-redux";
 
 export class TodoList extends Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
+        console.log(this.props.match)
+
+        const chart = this.props.match.path === '/archive'
+            ? <Chart data={[5,10,1,3]} size={[200,100]}/>
+            : null
 
         return (
             <React.Fragment>
-                <Chart data={[5,10,1,3]} size={[200,100]}/>
+                {chart}
                 <Task/>
                 <Task/>
                 <Task/>
@@ -16,3 +25,19 @@ export class TodoList extends Component {
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        newsList: state.newsList,
+        loading: state.loading
+    }
+}
+
+// function mapDispatchToProps(dispatch) {
+//     return {
+//         getNewsList: () => dispatch(getNewsList()),
+//         setMessage: (msg) => dispatch(setMessage(msg))
+//     }
+// }
+
+export default connect(mapStateToProps)(TodoList)
--- src/store/states/initialState.js
+++ src/store/states/initialState.js
@@ -1,5 +1,5 @@
 export const initialState = {
     error: null,
     loading: true,
-    message: 'asdfsd'
+    message: null
 }
